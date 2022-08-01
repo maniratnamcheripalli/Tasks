8e6920466c90129e902d65f62866979187abb5f9
maniratnamcheripalli <108408901+maniratnamcheripalli@users.noreply.github.com>
2022-08-01 13:12:01 +0530

    Update NeighbourShop.java

--- Oops tasks/Task2/src/com/ooptask2/NeighbourShop.java
+++ Oops tasks/Task2/src/com/ooptask2/NeighbourShop.java
@@ -2,7 +2,7 @@ package com.practise;
 
 public class NeighbourShop {
 void show() {
-		System.out.println("found jeans");
+		System.out.println("New varient in jeans");
 	}
 void jeans() {
 	System.out.println("out of stock");
